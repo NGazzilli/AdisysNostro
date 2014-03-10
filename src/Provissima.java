4afbdf345ac0039ce9c27aafa29ca81b7ea58ddf
GDivittorio <gm.divittorio@gmail.com>
2014-03-10 11:00:40 +0100

    Aggiunta altra classe
    bla.

---
+++ src/Provissima.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Divittorio - Tesi
+ */
+public class Provissima {
+    int bla;
+    int sbrigo;
+    
+}
